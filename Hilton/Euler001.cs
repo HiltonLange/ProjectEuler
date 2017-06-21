45b6e7b79224abf6fa6eeaed9fe89728328dd255
Hilton Lange <hilange@microsoft.com>
2017-06-21 13:11:21 -0700

    Hilton 1-1

---
+++ Hilton/Euler001.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Euler001
+    {
+        public static void Solve()
+        {
+            int ret = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0) ret += i;
+            }
+            Console.WriteLine(ret);
+        }
+    }
+}
